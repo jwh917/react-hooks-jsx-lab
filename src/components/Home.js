9f2514ec162ed34b43062f21dda9f6166bdcd2c6
Julien Hnery <jwh_917@yahoo.com>
2022-05-27 21:28:13 -0400

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <p style={{ color: "firebrick" }}>Home</p>
+    <h1 style={{ color: "firebrick" }}>{name} <br></br> is a Web Developer from {city}</h1>
+  </div>);
 }
 
 export default Home;
